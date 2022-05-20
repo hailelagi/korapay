bf6b53b53e1fb62945a3b06eb5a42db8dec92d18
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-05-20 04:23:18 +0100

    chore: get config from sys env

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,9 @@ import Config
 config :tesla, adapter: Tesla.Adapter.Hackney
 
 config :kora_pay,
-  public_key: nil,
-  private_key: nil
+  public_key: System.get_env("PUBLIC_KEY", nil),
+  private_key: System.get_env("PRIVATE_KEY", nil),
+  redirect_url: System.get_env("REDIRECT_URL", nil),
+  webhook_url: System.get_env("WEBHOOK_URL", nil)
 
 import_config "#{config_env()}.exs"
